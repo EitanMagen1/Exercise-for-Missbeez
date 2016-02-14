7b0e0d5d66f312ae472d7b54454838132fa6b9c6
EitanMagen1 <eitan.magen1@gmail.com>
2016-02-14 13:28:09 +0200

    Initial Commit

---
+++ Exercise for Missbeez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise for Missbeez
+//
+//  Created by Lauren Efron on 14/02/2016.
+//  Copyright © 2016 Eitan_Magen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
