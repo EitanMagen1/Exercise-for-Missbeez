398281e48bc54bd93890bd728bb8849959649479
EitanMagen1 <eitan.magen1@gmail.com>
2016-02-15 09:46:33 +0200

    first page real

---
+++ Exercise for Missbeez/MainTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainTableViewCell.swift
+//  Exercise for Missbeez
+//
+//  Created by Lauren Efron on 14/02/2016.
+//  Copyright © 2016 Eitan_Magen. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var pictureDescription: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+}
---
+++ Exercise for Missbeez/MainViewController.swift
@@ -0,0 +1,77 @@
+//
+//  MainViewController.swift
+//  Exercise for Missbeez
+//
+//  Created by Lauren Efron on 14/02/2016.
+//  Copyright © 2016 Eitan_Magen. All rights reserved.
+//
+
+import UIKit
+
+let cellIdentifier = "cellIdentifier"
+
+class MainViewController: UITableViewController    {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+      
+    }
+
+    private func setupNavigationBar() {
+        let label = UILabel(frame: CGRectMake(0, 0, 200, 30))
+        //label.font = UIFont.systemFontOfSize(21)
+        label.font = UIFont.boldSystemFontOfSize(21.0)
+        label.textColor = UIColor.darkGrayColor()
+        label.textAlignment = .Center
+        label.text = "Services For You"
+        
+        navigationItem.titleView = label
+    }
+    private func setupDescriptionText() {
+        let label = UILabel(frame: CGRectMake(0, 0, 200, 30))
+        //label.font = UIFont.systemFontOfSize(21)
+        label.font = UIFont.boldSystemFontOfSize(21.0)
+        label.textColor = UIColor.darkGrayColor()
+        label.textAlignment = .Center
+       // label.text = "Services For You"
+        
+        navigationItem.titleView = label
+    }
+
+    let itemDescription = ["Manicure","Pedicure","Hair treatment","Specials"]
+    // MARK: - Table View Delegate and Data Source
+    override func tableView(tableView: UITableView,
+        heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return (self.tableView.frame.height - 64 )/4
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MainTableViewCell
+        cell.photoImageView.image = imageAtIndex(indexPath.row)
+        cell.pictureDescription.textColor = UIColor.whiteColor()
+        cell.pictureDescription.text = itemDescription[indexPath.row]
+        
+        return cell
+
+    }
+
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        print("1")
+    }
+    
+    func imageAtIndex(index: Int) -> UIImage? {
+        return UIImage(named: String(index))
+    }
+
+}
+
